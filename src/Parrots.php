a521c17e32cc0fde962d4a2b02f15675d5c06296
Ben Peachey <potherca@gmail.com>
2015-03-16 21:53:43 +0100

    Creates controller class to be used from delivery mechanisms (web, cli, etc).

---
+++ src/Parrots.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Potherca\Parrots;
+
+use Potherca\Parrots\Transformers\TransformerInterface;
+
+class Parrots extends AbstractData
+{
+    ////////////////////////////// CLASS PROPERTIES \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    /** @var TransformerInterface */
+    private $m_oTransformer;
+    /** @var array */
+    private $m_aSupportedExtensions = array(
+        'html' => 'text/html',
+        'json' => 'application/json',
+        'txt' => 'text/plain',
+    );
+
+    //////////////////////////// SETTERS AND GETTERS \\\\\\\\\\\\\\\\\\\\\\\\\\\
+    /**
+     * @param array $m_aSupportedExtensions
+     */
+    public function setSupportedExtensions($m_aSupportedExtensions)
+    {
+        $this->m_aSupportedExtensions = $m_aSupportedExtensions;
+    }
+
+    /** @param TransformerInterface $p_oTransformer*/
+    final public function setTransformer($p_oTransformer)
+    {
+        $this->m_oTransformer = $p_oTransformer;
+    }
+
+    //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    final public function __construct($p_aData)
+    {
+        $this->setFromArray($p_aData);
+        $this->setTypeFromSubject();
+    }
+
+    final public function parrot()
+    {
+        $transformer = $this->m_oTransformer;
+        $transformer->setFromArray([
+            self::PROPERTY_BACKGROUND_COLOR => $this->getBackgroundColor(),
+            self::PROPERTY_COLOR => $this->getColor(),
+            self::PROPERTY_PREFIX => $this->getPrefix(),
+            self::PROPERTY_SUBJECT => $this->getSubject(),
+            self::PROPERTY_TYPE => $this->getType(),
+        ]);
+        $sResult = $transformer->transform();
+
+        return $sResult;
+    }
+
+    ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    /**
+     * @return string
+     */
+    private function setTypeFromSubject()
+    {
+        $aParts = explode('.', $this->m_sSubject);
+        $iCount = count($aParts);
+        if ($iCount > 1
+            && in_array($aParts[$iCount-1], array_keys($this->m_aSupportedExtensions))
+        ) {
+            $this->m_sType = $this->m_aSupportedExtensions[array_pop($aParts)];
+            $this->m_sSubject = implode('.', $aParts);
+        }
+    }
+}
+
+/*EOF*/
